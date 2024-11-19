4bab46ddccfe6ae6e58ee5062ee23ec73f43ec91
1029920178@qq.com <1029910278@qq.com>
2024-11-19 16:55:14 +0800

    feat: add config file for store interface/setting

---
+++ VirtualStreetSnap/Models/AppConfig.cs
@@ -0,0 +1,29 @@
+﻿namespace VirtualStreetSnap.Models;
+
+public class Settings
+{
+    public required string SaveDirectory { get; set; }
+    public required string FilePrefix { get; set; } = "IMG";
+}
+
+public class Overlays
+{
+    public required Guides Guides { get; set; }
+    public bool Focus { get; set; } = true;
+}
+
+public class Guides
+{
+    public bool Grid { get; set; } = true;
+    public bool Center { get; set; }
+    public bool Ratio { get; set; }
+    
+    public float Opacity { get; set; } = 0.5f;
+}
+
+public class AppConfig
+{
+    public required string Version { get; set; } = "0.0.1";
+    public required Settings Settings { get; set; }
+    public required Overlays Overlays { get; set; }
+}
--- VirtualStreetSnap/ViewModels/MainWindowViewModel.cs
+++ VirtualStreetSnap/ViewModels/MainWindowViewModel.cs
@@ -1,16 +1,20 @@
 ﻿using System;
 using System.Collections.ObjectModel;
+using System.IO;
 using System.Linq;
 using Avalonia.Controls;
 using Avalonia.Media;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using VirtualStreetSnap.Models;
+using VirtualStreetSnap.Services;
 
 namespace VirtualStreetSnap.ViewModels;
 
 public partial class MainWindowViewModel : ViewModelBase
 {
+    private static readonly string configFilePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "config.json");
+
     [ObservableProperty]
     private SizeRadio? _selectedSizeRadio;
 
@@ -18,7 +22,7 @@ public partial class MainWindowViewModel : ViewModelBase
     private bool _showFocusBorder = true;
 
     [ObservableProperty]
-    private bool _showGuideLinesThirds = true;
+    private bool _showGuideLinesGrid = true;
 
     [ObservableProperty]
     private bool _showGuideLinesCenter;
@@ -31,6 +35,16 @@ public partial class MainWindowViewModel : ViewModelBase
 
     [ObservableProperty]
     private Color _borderColor = Colors.Brown;
+
+    [ObservableProperty]
+    private AppConfig _config;
+
+    [ObservableProperty]
+    private string _filePrefix = "IMG";
+
+    [ObservableProperty]
+    private string _saveDirectory = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile),
+        "Pictures", "VirtualStreetSnap");
 
     public ObservableCollection<SizeRadio> RadioItems { get; } =
     [
@@ -45,12 +59,33 @@ public partial class MainWindowViewModel : ViewModelBase
     public MainWindowViewModel()
     {
         SelectedSizeRadio = RadioItems.First();
+        Console.WriteLine(configFilePath);
+        var configService = new ConfigService(configFilePath);
+        Config = configService.LoadConfig();
+        if (Config is null) return;
+        ShowGuideLinesGrid = Config.Overlays.Guides.Grid;
+        ShowGuideLinesCenter = Config.Overlays.Guides.Center;
+        ShowGuideLinesRatio = Config.Overlays.Guides.Ratio;
+        ShowFocusBorder = Config.Overlays.Focus;
+        SaveDirectory = Config.Settings.SaveDirectory;
+        FilePrefix = Config.Settings.FilePrefix;
+        // Set the default values if the config is not existing
+        configService.SaveConfig(Config);
     }
 
     [RelayCommand]
     public void OnCloseButtonClick()
     {
         if (Design.IsDesignMode) return;
+        // Save the config before closing the app
+        Config.Overlays.Guides.Grid = ShowGuideLinesGrid;
+        Config.Overlays.Guides.Center = ShowGuideLinesCenter;
+        Config.Overlays.Guides.Ratio = ShowGuideLinesRatio;
+        Config.Overlays.Focus = ShowFocusBorder;
+        Config.Settings.SaveDirectory = SaveDirectory;
+        Config.Settings.FilePrefix = FilePrefix;
+        var configService = new ConfigService(configFilePath);
+        configService.SaveConfig(Config);
         Environment.Exit(0);
     }
 }
