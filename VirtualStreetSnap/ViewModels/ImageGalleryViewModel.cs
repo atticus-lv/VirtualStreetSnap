affa7666c0728ade219ebcbdbbdceec03fbc7c48
1029920178@qq.com <1029910278@qq.com>
2024-11-21 04:11:33 +0800

    feat: image gallery

---
+++ VirtualStreetSnap/ViewModels/ImageGalleryViewModel.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.IO;
+using Avalonia.Media.Imaging;
+using CommunityToolkit.Mvvm.ComponentModel;
+using VirtualStreetSnap.Models;
+using VirtualStreetSnap.Services;
+
+namespace VirtualStreetSnap.ViewModels;
+
+public partial class ImageGalleryViewModel : ViewModelBase
+{
+    [ObservableProperty]
+    private ObservableCollection<ImageThumbViewModel> _thumbnails = new();
+
+    [ObservableProperty]
+    private ImageThumbViewModel? _selectedThumbnail;
+
+    [ObservableProperty]
+    private Bitmap? _selectedImage;
+    
+    [ObservableProperty]
+    private string _selectedImageName = "";
+
+    [ObservableProperty]
+    private AppConfig _config = ConfigService.Instance;
+
+    public ImageGalleryViewModel()
+    {
+        LoadThumbnails(Config.Settings.SaveDirectory);
+        Config.Settings.PropertyChanged += OnSettingsPropertyChanged;
+    }
+
+    private void OnSettingsPropertyChanged(object? sender, PropertyChangedEventArgs e)
+    {
+        if (e.PropertyName == nameof(Settings.SaveDirectory))
+        {
+            LoadThumbnails(Config.Settings.SaveDirectory);
+        }
+    }
+
+    public void LoadThumbnails(string directoryPath)
+    {
+        if (!Directory.Exists(directoryPath)) return;
+        var pngFiles = Directory.GetFiles(directoryPath, "*.png");
+        Thumbnails.Clear();
+        foreach (var file in pngFiles) Thumbnails.Add(new ImageThumbViewModel(file));
+    }
+
+    partial void OnSelectedThumbnailChanged(ImageThumbViewModel? value)
+    {
+        SelectedImage = value?.Image;
+        SelectedImageName = value?.ImgName ?? "Unknown";
+    }
+}
---
+++ VirtualStreetSnap/ViewModels/ImageThumbViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using Avalonia.Media.Imaging;
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace VirtualStreetSnap.ViewModels;
+
+public partial class ImageThumbViewModel : ViewModelBase
+{
+    private const string DefaultImagePath = "avares://VirtualStreetSnap/Assets/avalonia-logo.ico";
+
+    [ObservableProperty]
+    private string _imgPath = "";
+
+    public string ImgName;
+    
+    [ObservableProperty]
+    private Bitmap _image;
+
+    public ImageThumbViewModel(string imgPath)
+    {
+        ImgPath = File.Exists(imgPath) ? imgPath : DefaultImagePath;
+        ImgName = Path.GetFileName(ImgPath);
+        LoadImage(ImgPath);
+    }
+
+
+    private void LoadImage(string imagePath)
+    {
+        try
+        {
+            Image = new Bitmap(imagePath);
+        }
+        catch (Exception)
+        {
+            Image = new Bitmap(DefaultImagePath);
+        }
+    }
+}
