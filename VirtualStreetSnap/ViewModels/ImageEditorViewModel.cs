08fc7472586507714125c914966ffbbe2a36d00f
1029920178@qq.com <1029910278@qq.com>
2024-11-27 08:24:52 +0800

    imageEditor: base ui / viewmodel

---
+++ VirtualStreetSnap/ViewModels/ImageEditorViewModel.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+using Avalonia.Media.Imaging;
+using Avalonia.Platform;
+using CommunityToolkit.Mvvm.ComponentModel;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSharp.Processing;
+using VirtualStreetSnap.Models;
+using VirtualStreetSnap.Services;
+
+namespace VirtualStreetSnap.ViewModels;
+
+public partial class ImageEditorViewModel : ViewModelBase
+{
+    private const string DefaultImagePath = "avares://VirtualStreetSnap/Assets/avalonia-logo.ico";
+
+    [ObservableProperty]
+    private ImageViewerViewModel _editImageViewer = new();
+
+    [ObservableProperty]
+    private LayerManagerViewModel _layerManager = new();
+
+    [ObservableProperty]
+    private LayerBaseViewModel? _selectedLayer;
+
+    public ImageEditorViewModel()
+    {
+        var initialImage = new ImageBase
+        {
+            Image = new Bitmap(AssetLoader.Open(new Uri(DefaultImagePath)))
+        };
+        EditImageViewer.ViewImage = initialImage;
+        LayerManager.InitialImage = ImageEditHelper.ConvertToImageSharp(initialImage.Image);
+
+        // Add initial layers
+        LayerManager.AddLayer(new BrightnessContrastLayerViewModel { Name = "Brightness/Contrast" });
+        LayerManager.AddLayer(new SharpnessLayerViewModel { Name = "Sharpness" });
+        LayerManager.AddLayer(new HslLayerViewModel { Name = "HSL" });
+
+        // Set the first layer as selected
+        SelectedLayer = LayerManager.Layers.FirstOrDefault();
+    }
+}
+
+public class LayerManagerViewModel : ViewModelBase
+{
+    public Image<Rgba32> InitialImage { get; set; }
+
+    public ObservableCollection<LayerBaseViewModel> Layers { get; set; } = new();
+
+    public void AddLayer(LayerBaseViewModel layer)
+    {
+        Layers.Add(layer);
+        layer.LayerModified += RefreshFinalImage;
+        layer.PropertyChanged += (sender, args) =>
+        {
+            if (args.PropertyName == nameof(LayerBaseViewModel.IsVisible)) RefreshFinalImage();
+        };
+    }
+
+    public void RemoveLayer(LayerBaseViewModel layer)
+    {
+        Layers.Remove(layer);
+        layer.LayerModified -= RefreshFinalImage;
+    }
+
+    public void HideLayer(LayerBaseViewModel layer)
+    {
+        layer.IsVisible = false;
+    }
+
+    public void ShowLayer(LayerBaseViewModel layer)
+    {
+        layer.IsVisible = true;
+    }
+
+    public Bitmap DisplayImage
+    {
+        get
+        {
+            var finalImage = GenerateFinalImage();
+            return finalImage != null ? ImageEditHelper.ConvertToBitmap(finalImage) : null;
+        }
+    }
+
+    private void RefreshFinalImage()
+    {
+        foreach (var layer in Layers)
+        {
+            layer.ApplyModifiers();
+        }
+        OnPropertyChanged(nameof(DisplayImage));
+    }
+
+    private Image<Rgba32> GenerateFinalImage()
+    {
+        if (Layers.Count == 0 || InitialImage == null) return null;
+        var finalImage = InitialImage.Clone();
+        foreach (var layer in Layers)
+        {
+            layer.InitialImage = finalImage.Clone();
+            layer.ApplyModifiers();
+            if (layer.IsVisible) finalImage.Mutate(x => x.DrawImage(layer.ModifiedImage, 1));
+        }
+
+        return finalImage;
+    }
+}
---
+++ VirtualStreetSnap/ViewModels/ImageLayerViewModel.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+using VirtualStreetSnap.Services;
+
+namespace VirtualStreetSnap.ViewModels;
+
+public class ImageLayerViewModel
+{ }
+
+public class SliderModel : ViewModelBase
+{
+    public string Name { get; set; }
+    public float Value { get; set; }
+    public float MinValue { get; set; }
+    public float MaxValue { get; set; }
+    public Action<float> OnChange { get; set; }
+
+    private float _currentValue;
+
+    public float CurrentValue
+    {
+        get => _currentValue;
+        set
+        {
+            if (SetProperty(ref _currentValue, value))
+            {
+                OnChange?.Invoke(value);
+            }
+        }
+    }
+}
+
+public abstract class LayerBaseViewModel : ViewModelBase
+{
+    public string Name { get; set; }
+    public bool IsVisible { get; set; } = true;
+    public Image<Rgba32> InitialImage { get; set; }
+    public Image<Rgba32> ModifiedImage { get; set; }
+    public ObservableCollection<SliderModel> Sliders { get; set; } = new ObservableCollection<SliderModel>();
+
+    public event Action LayerModified;
+
+    protected void OnLayerModified()
+    {
+        LayerModified?.Invoke();
+    }
+
+    public abstract void ApplyModifiers();
+}
+
+public class BrightnessContrastLayerViewModel : LayerBaseViewModel
+{
+    public float Brightness { get; set; } = 1.0f;
+    public float Contrast { get; set; } = 1.0f;
+
+    public BrightnessContrastLayerViewModel()
+    {
+        Sliders.Add(new SliderModel
+        {
+            Name = "Brightness",
+            MinValue = 0.0f,
+            MaxValue = 2.0f,
+            Value = Brightness,
+            OnChange = value =>
+            {
+                Brightness = value;
+                OnLayerModified();
+            }
+        });
+
+        Sliders.Add(new SliderModel
+        {
+            Name = "Contrast",
+            MinValue = 0.0f,
+            MaxValue = 2.0f,
+            Value = Contrast,
+            OnChange = value =>
+            {
+                Contrast = value;
+                OnLayerModified();
+            }
+        });
+    }
+
+    public override void ApplyModifiers()
+    {
+        if (IsVisible && InitialImage != null)
+        {
+            ModifiedImage = InitialImage.Clone();
+            ImageEditHelper.ApplyBrightnessContrast(ModifiedImage, Brightness, Contrast);
+        }
+    }
+}
+
+public class SharpnessLayerViewModel : LayerBaseViewModel
+{
+    public float Sharpness { get; set; } = 0.0f;
+
+    public SharpnessLayerViewModel()
+    {
+        Sliders.Add(new SliderModel
+        {
+            Name = "Sharpness",
+            MinValue = 0.0f,
+            MaxValue = 10.0f,
+            Value = Sharpness,
+            OnChange = value =>
+            {
+                Sharpness = value;
+                OnLayerModified();
+            }
+        });
+    }
+
+    public override void ApplyModifiers()
+    {
+        if (IsVisible && InitialImage != null)
+        {
+            ModifiedImage = InitialImage.Clone();
+            ImageEditHelper.ApplySharpness(ModifiedImage, Sharpness);
+        }
+    }
+}
+
+public class HslLayerViewModel : LayerBaseViewModel
+{
+    public float Hue { get; set; } = 0.0f;
+    public float Saturation { get; set; } = 1.0f;
+    public float Lightness { get; set; } = 1.0f;
+
+    public HslLayerViewModel()
+    {
+        Sliders.Add(new SliderModel
+        {
+            Name = "Hue",
+            MinValue = -180.0f,
+            MaxValue = 180.0f,
+            Value = Hue,
+            OnChange = value =>
+            {
+                Hue = value;
+                OnLayerModified();
+            }
+        });
+
+        Sliders.Add(new SliderModel
+        {
+            Name = "Saturation",
+            MinValue = 0.0f,
+            MaxValue = 2.0f,
+            Value = Saturation,
+            OnChange = value =>
+            {
+                Saturation = value;
+                OnLayerModified();
+            }
+        });
+
+        Sliders.Add(new SliderModel
+        {
+            Name = "Lightness",
+            MinValue = 0.0f,
+            MaxValue = 2.0f,
+            Value = Lightness,
+            OnChange = value =>
+            {
+                Lightness = value;
+                OnLayerModified();
+            }
+        });
+    }
+
+    public override void ApplyModifiers()
+    {
+        if (IsVisible && InitialImage != null)
+        {
+            ModifiedImage = InitialImage.Clone();
+            ImageEditHelper.ApplyHsl(ModifiedImage, Hue, Saturation, Lightness);
+        }
+    }
+}
