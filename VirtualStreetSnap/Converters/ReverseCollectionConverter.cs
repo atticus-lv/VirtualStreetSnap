00e5159351784610b51b3de48208651196567180
1029920178@qq.com <1029910278@qq.com>
2024-11-27 08:28:55 +0800

    fix: set ReverseCollectionConverter fallback

--- VirtualStreetSnap/Converters/ReverseCollectionConverter.cs
+++ VirtualStreetSnap/Converters/ReverseCollectionConverter.cs
@@ -19,6 +19,6 @@ public class ReverseCollectionConverter : IValueConverter
 
     public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
     {
-        throw new NotImplementedException();
+        return value;
     }
 }
