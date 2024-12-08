099432e6f6016bffef79543a5d9bf770ebd1c2d0
1029920178@qq.com <1029910278@qq.com>
2024-12-08 16:24:48 +0800

    refactor: settings view with new custom setting card; bump to 0.1.5.2

---
+++ src/VirtualStreetSnap/Controls/SettingsCard.axaml.cs
@@ -0,0 +1,27 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace VirtualStreetSnap.Controls;
+
+public partial class SettingsCard : ContentControl
+{
+    public static readonly StyledProperty<string> SettingNameProperty =
+        AvaloniaProperty.Register<SettingsCard, string>(nameof(SettingName));
+
+    public string SettingName
+    {
+        get { return GetValue(SettingNameProperty); }
+        set { SetValue(SettingNameProperty, value); }
+    }
+
+    public SettingsCard()
+    {
+        InitializeComponent();
+    }
+
+    private void InitializeComponent()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+}
--- src/VirtualStreetSnap/Program.cs
+++ src/VirtualStreetSnap/Program.cs
@@ -1,5 +1,8 @@
 ﻿using Avalonia;
 using System;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
 
 namespace VirtualStreetSnap;
 
@@ -9,8 +12,17 @@ sealed class Program
     // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
     // yet and stuff might break.
     [STAThread]
-    public static void Main(string[] args) => BuildAvaloniaApp()
-        .StartWithClassicDesktopLifetime(args);
+    public static void Main(string[] args)
+    {
+        Trace.Listeners.Add(new TextWriterTraceListener("VirtualStreetSnap.log"));
+        Trace.AutoFlush = true;
+        TextWriter originalConsoleOutput = Console.Out;
+        MultiTextWriter multiTextWriter = new MultiTextWriter(originalConsoleOutput, new TraceTextWriter());
+        Console.SetOut(multiTextWriter);
+
+        BuildAvaloniaApp()
+            .StartWithClassicDesktopLifetime(args);
+    }
 
     // Avalonia configuration, don't remove; also used by visual designer.
     public static AppBuilder BuildAvaloniaApp()
@@ -18,4 +30,45 @@ sealed class Program
             .UsePlatformDetect()
             .WithInterFont()
             .LogToTrace();
+}
+
+public class MultiTextWriter : TextWriter
+{
+    private readonly TextWriter _consoleWriter;
+    private readonly TextWriter _traceWriter;
+
+    public MultiTextWriter(TextWriter consoleWriter, TextWriter traceWriter)
+    {
+        _consoleWriter = consoleWriter;
+        _traceWriter = traceWriter;
+    }
+
+    public override Encoding Encoding => Encoding.UTF8;
+
+    public override void WriteLine(string value)
+    {
+        _consoleWriter.WriteLine(value);
+        _traceWriter.WriteLine(value);
+    }
+
+    public override void Write(string value)
+    {
+        _consoleWriter.Write(value);
+        _traceWriter.Write(value);
+    }
+}
+
+public class TraceTextWriter : TextWriter
+{
+    public override Encoding Encoding => Encoding.UTF8;
+
+    public override void WriteLine(string value)
+    {
+        Trace.WriteLine(value);
+    }
+
+    public override void Write(string value)
+    {
+        Trace.Write(value);
+    }
 }
